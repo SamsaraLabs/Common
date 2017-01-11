07a80922300c8b818c0e8292bbd45d6dd312a0bc
JordanRL <jordan.ledoux@gmail.com>
2017-01-10 21:14:35 -0800

    Updated OptionalExit

--- src/Samsara/Exceptions/UsageError/OptionalExit.php
+++ src/Samsara/Exceptions/UsageError/OptionalExit.php
@@ -2,9 +2,17 @@
 
 namespace Samsara\Exceptions\UsageError;
 
+use Exception;
 use Samsara\Exceptions\Base\UsageError;
 
 class OptionalExit extends UsageError
 {
-    
+
+    public function __construct($reason, $message = "", $code = 0, Exception $previous = null)
+    {
+        $message = 'Message: '.$message.' | Reason: '.$reason;
+
+        parent::__construct($message, $code, $previous);
+    }
+
 }
